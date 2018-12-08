22040ddee0339288222f49adce41235c49db5e93
Joseph Ferro <joejoebeef@gmail.com>
2018-12-08 11:06:19 -0700

    add locations controller

--- app/controllers/api/locations_controller.rb
+++ app/controllers/api/locations_controller.rb
@@ -1,2 +1,32 @@
 class Api::LocationsController < ApplicationController
+  
+  def index
+    render json: Location.all
+  end
+
+  def create
+    location = Location.new(location_params)
+    if location.save
+      render json: location
+    else
+      render json: { errors: location.errors }
+    end
+  end
+
+  def update
+    location = Location.find(params[:id])
+    location.update
+    render json: location
+  end
+
+  def destroy
+    Location.find(params[:id]).destroy
+    render json: {message: "Location Deleted"}
+  end
+
+  private
+    def location_params
+      params.require(:location).permit(:name, :days, :trip_id)
+    end
+    
 end
