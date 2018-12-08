fd76e611ce479343f94d85d5e4020c0da6079113
Joseph Ferro <joejoebeef@gmail.com>
2018-12-08 15:55:02 -0700

    fixed git conflicts

--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { Form, Button, Segment } from "semantic-ui-react";
-
+import { Form, Button, Segment, Divider } from "semantic-ui-react";
+  
 class TripForm extends React.Component {
   state = { name: "", startDate: "", endDate: "" };
 
-  handleSubmit = e => {
+   handleSubmit = e => {
     e.preventDefault();
     this.props.add(this.state.name);
     this.setState({ name: "" });
   };
 
-  handleChange = e => {
+   handleChange = e => {
     this.setState({ name: e.target.value });
   };
 
   render() {
     return (
       <Segment inverted>
+        <Divider horizontal inverted>
+          NEW TRIP
+        </Divider>
         <Form inverted onSubmit={this.handleSubmit}>
           <Form.Field>
             <input
@@ -27,7 +30,7 @@ class TripForm extends React.Component {
               onChange={this.handleChange}
             />
           </Form.Field>
-          <Form.Group inline>
+          <Form.Group widths = 'equal'>
             <Form.Field>
               <input placeholder="Start Date" />
             </Form.Field>
@@ -35,21 +38,11 @@ class TripForm extends React.Component {
               <input placeholder="End Date" />
             </Form.Field>
           </Form.Group>
-
-          {/* <Form.Field>
-          <input placeholder='City' />
-        </Form.Field>
-        <Form.Field>
-          <input placeholder='State' />
-        </Form.Field>
-        <Form.Field>
-          <input placeholder='Zip' />
-        </Form.Field>  */}
-          <Button type="submit">Submit</Button>
+          <Divider />
+          <Button fluid type='submit' color = 'blue'>Submit</Button>
         </Form>
       </Segment>
-    );
-  }
+    )};
 }
 
 export default TripForm;
