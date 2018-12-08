52b902deea006444b480c435715bef962f6fec45
Joseph Ferro <joejoebeef@gmail.com>
2018-12-08 14:25:18 -0700

    add form styling and form for trip

--- client/src/components/Trip.js
+++ client/src/components/Trip.js
@@ -1,19 +1,21 @@
 import React from "react";
-import { Container, Header, Segment, Icon } from "semantic-ui-react";
+import { Container, Header, Segment, Icon, Button } from "semantic-ui-react";
 
 class Trip extends React.Component {
   render() {
     return (
-      <Container style={{ padding: "25px" }}>
+      <Container style={{ marginTop: "10px" }}>
         <Segment secondary>
           <Header as="h3">
-            <Icon name="map marker" color="blue" />
-            <h1>{this.props.tripName}</h1>
+            <div>
+             <Icon name="map marker" color="blue" />
+             <p>{this.props.tripName}</p>
+            </div>
           </Header>
         </Segment>
       </Container>
     ); //end of return
-  } //end of render
+  } 
 } //end of class Trip
 
 export default Trip;
--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -1,32 +1,35 @@
 import React from 'react';
-import { Form, Button, Segment, } from 'semantic-ui-react';
+import { Form, Button, Segment, Divider, } from 'semantic-ui-react';
 
 const TripForm = () => (
   <Segment inverted>
-  <Form inverted>
-    <Form.Field>
-      <input placeholder='Trip Name' />
-    </Form.Field>
-    <Form.Group inline>
+    <Divider horizontal inverted>
+      NEW TRIP
+    </Divider>
+    <Form inverted>
       <Form.Field>
-        <input placeholder='Start Date' />
+        <input placeholder='Trip Name' />
+      </Form.Field>
+      <Form.Group widths = 'equal'>
+        <Form.Field>
+          <input fluid placeholder='Start Date' />
+        </Form.Field>
+        <Form.Field>
+          <input fluid placeholder='End Date' />
+        </Form.Field>
+      </Form.Group>
+      <Divider />
+      {/* <Form.Field>
+        <input placeholder='City' />
       </Form.Field>
       <Form.Field>
-        <input placeholder='End Date' />
+        <input placeholder='State' />
       </Form.Field>
-    </Form.Group>
-    
-    {/* <Form.Field>
-      <input placeholder='City' />
-    </Form.Field>
-    <Form.Field>
-      <input placeholder='State' />
-    </Form.Field>
-    <Form.Field>
-      <input placeholder='Zip' />
-    </Form.Field>  */}
-    <Button type='submit'>Submit</Button>
-  </Form>
+      <Form.Field>
+        <input placeholder='Zip' />
+      </Form.Field>  */}
+      <Button fluid type='submit' color = 'blue'>Submit</Button>
+    </Form>
   </Segment>
 )
 
